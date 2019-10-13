2e5e66d855f8dd296545fe9f9fd5cc6337dec3d9
carlaferrarez <carlacristinef@gmail.com>
2019-10-13 14:37:36 -0300

    adicionando css

--- index.php
+++ index.php
@@ -72,10 +72,96 @@ exec ('gpio -x mcp3004:100:0 aread 100', $p0);
 // colocar 24 horas grafico, notificacao, diferenciar dia e noite, validar com outro aparelho, trabalhar pessoas sedentaria e atleta, na propria pessoa, fazer algo movel, fazer testes com temperatura
 
 echo ' 
-<html>
-<meta http-equiv="refresh" content="1" />
-<img src="teste.png" title="Graph 2" />
-<img src="teste2.png" title="Graph 2" />
+<html lang="en">
+<head>
+	<title>Palpita</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="1" />
+	<!-- <meta http-equiv="refresh" content="1" /> -->
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/cardio.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+	<div class="contact1">
+		<div class="container-contact1">
+			<div>
+				<div class="contact1-pic js-tilt" data-tilt style="margin-bottom: 40px; display:block; margin: auto;">
+					<img src="images/cardio.png" alt="IMG">
+				</div>
+	
+				<div style="display: grid; margin-bottom: 40px;">
+					<img src="teste.png" title="Graph" />
+					<img src="teste2.png" title="Graph 2" />
+				</div>
+			</div>
+			
+
+			<form class="contact1-form validate-form">
+				<span class="contact1-form-title">
+					How about measuring your heart rate?
+				</span>
+
+				<div class="wrap-input1 validate-input" data-validate = "Name is required">
+					<input class="input1" type="text" name="name" placeholder="Enter heart rate limit">
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<input class="input1" type="text" name="email" placeholder="Enter your e-mail">
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="container-contact1-form-btn">
+					<button class="contact1-form-btn">
+						<span>
+							Send Email
+							<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div style="font-size: 10px; background-color: #00c6ff;">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
+
+
+
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$(".js-tilt").tilt({
+			scale: 1.1
+		})
+	</script>
+
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
 </html>
 ';
 ?>
